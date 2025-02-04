c926d156b92cd691e2870b9b7bf5d43ac2eaf7e0
Rythmat <teotaniwha@gmail.com>
2025-02-03 21:27:42 -0700

    Puppy data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import { puppyList } from './data.js'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
-      
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+      }
     </>
   )
 }
